607f5a2710c5d2c3dac30cb3672b960eed336577
Mark Cronje <markcronje@Marks-MacBook-Air.local>
2016-06-13 01:17:04 +1000

    CRUD functionality complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,41 @@
 class PostsController < ApplicationController
 
 def index
-  @posts = Post.all 
+  @posts = Post.all
 end
 
 def new
   @post = Post.new
 end
 
+def edit
+   @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post = Post.update(post_params)
+  redirect_to(post_path(@post))
+end
+
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end
+
+
 
 def create
   @post = Post.create(post_params)
   redirect_to posts_path
 end
+
+def show
+  @post = Post.find(params[:id])
+end
+
 
 private
 
